e8b4db218382fd7933c474078348a5f94afed4b5
Ameka Brown <brownameka05@gmail.com>
2018-09-18 11:45:12 -0500

    Learned functions and hello python language

---
+++ function.py
@@ -0,0 +1,10 @@
+def evenOrOdd(number):
+    if(number % 2 == 0):
+        print("EVEN")
+    else:
+        print("odd")
+
+evenOrOdd(7)
+
+
+evenOrOdd(6)
---
+++ hello.py
@@ -0,0 +1,9 @@
+first_name = input ("Enter first name:")
+last_name = input ("Enter last name:")
+city = input ("Enter city here:")
+state = input ("Enter state:")
+zip_code = input ("Enter Zip code:")
+
+message = "My name is {0}, {1} and I live in {2}, {3} {4}".format(first_name,last_name,city,state,zip_code)
+
+print(message)
